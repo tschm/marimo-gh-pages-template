97f5a275087af8a46e2b05e47cb7c5b35f5a0041
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-12 12:00:16 +0400

    Update pandas_penguins.py

--- notebooks/pandas_penguins.py
+++ notebooks/pandas_penguins.py
@@ -3,30 +3,27 @@ import marimo
 __generated_with = "0.13.15"
 app = marimo.App(width="medium")
 
-
-@app.cell
-def _():
+with app.setup:
     import marimo as mo
     import pandas as pd
     import polars as pl
-    return mo, pd, pl
 
 
 @app.cell
-def _(mo):
+def _():
     _file = mo.notebook_location() / "public" / "penguins.csv"
     file_str = str(_file)
     return (file_str,)
 
 
 @app.cell
-def _(file_str, pd):
+def _(file_str):
     pd.read_csv(file_str)
     return
 
 
 @app.cell
-def _(file_str, pl):
+def _(file_str):
     pl.read_csv(file_str)
     return
 
